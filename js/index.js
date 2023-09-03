b47ff36901515634d7952ec0cd7d696469b0ae3f
Noibi Jamiu Gbolahan <gboljamiu207@gmail.com>
2023-09-03 23:28:33 +0100

    update to smooth scrolling

--- js/index.js
+++ js/index.js
@@ -10,28 +10,28 @@ btnEl.addEventListener("click", function () {
 
 const allLInk = document.querySelectorAll("a");
 
-allLInk.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
+// allLInk.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+// const href = link.getAttribute("href");
 
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
+// if (href === "#") {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+// }
 
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+// if (href !== "#" && href.startsWith("#")) {
+//   const sectionEl = document.querySelector(href);
+//   sectionEl.scrollIntoView({ behavior: "smooth" });
+// }
 
-    if (link.classList.contains("nav-item")) {
-      headerEl.classList.toggle("nav-open");
-    }
-  });
-});
+// if (link.classList.contains("nav-item")) {
+//   headerEl.classList.toggle("nav-open");
+// }
+//   });
+// });
 
 //////////////////////////////////////
 // Sticky nav
